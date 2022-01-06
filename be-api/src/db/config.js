b94276fb02f0c553d0d8c3149eb24fd494229d5b
Nico Rithner <nicorithner@gmail.com>
2022-01-06 07:00:46 -0700

    Edit db/config.js

--- be-api/src/db/config.js
+++ be-api/src/db/config.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    database: "google-oauth",
-    use_env_variable: "DATABASE_DEV_URL",
-    dialect: "postgres",
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_DATABASE || 'google-oauth',
+    dialect: 'postgres',
   },
   production: {
-    database: "google-oauth",
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    database: 'google-oauth',
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
