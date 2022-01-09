80c3cbaf41ff001810efd31a5a0e6bc92670f540
Nico Rithner <nicorithner@gmail.com>
2022-01-08 18:22:17 -0700

    Connect google-oauth. Change to port 8081

--- google-oauth-client/src/App.js
+++ google-oauth-client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import axios from 'axios';
+import Cookie from 'js-cookie';
+import { GoogleLogin } from 'react-google-login';
+export const axiosApiCall = (url, method, body = {}) => axios({
+    method,
+    url: `${process.env.REACT_APP_API_BASE_URL}${url}`,
+    data: body,
+  });
+export default function App() {
+  const onGoogleSuccess = (response) => {
+    const access_token = response.accessToken;
+    axiosApiCall(
+      '/auth/google',
+      'post',
+      { access_token }
+    ).then((res) => {
+      const { user, token } = res.data;
+      // Save the JWT inside a cookie
+      Cookie.set('token', token);
+    }).catch((err) => {
+      throw new Error(err);
+    })
+  }
+  const onGoogleFailure = () => {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#151a30',
+      color: 'white'
+    }}>
+      <h1>Google Oauth Sign In</h1>
+      <GoogleLogin 
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Sign in with Google"
+        onSuccess={onGoogleSuccess}
+        onFailure={onGoogleFailure}
+        className="google-login-button" />
     </div>
   );
 }
-
-export default App;
